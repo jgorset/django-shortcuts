839bbea6000a7157e82c843d2f4749d24244279f
Johannes Gorset <jgorset@gmail.com>
2011-04-29 11:32:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Django shortcuts
+
+## About
+
+Ruby on Rails-like shortcuts for Django.
+
+## Usage
+
+    $ python manage.py runserver
+    $ django runserver
+    $ django r
+    
+    $ python manage.py shell
+    $ django shell
+    $ django s
+
+## Installation
+
+    $ pip install django-shortcuts
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'django-shortcuts',
+  version = '0.1',
+  description = "Ruby on Rails-like shortcuts for Django.",
+  author = "Johannes Gorset",
+  author_email = "jgorset@gmail.com",
+  url = "http://github.com/jgorset/django-shortcuts",
+  scripts = ['scripts/django']
+)
