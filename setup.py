07a8b9ede17fdeb3aacbad5d11cb75d3ea98927e
Johannes <jgorset@gmail.com>
2011-05-22 20:46:28 +0200

    Change description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'django-shortcuts',
   version = '0.2',
-  description = "Ruby on Rails-like shortcuts for Django.",
+  description = "You spend way too much time typing 'python manage.py'",
   author = "Johannes Gorset",
   author_email = "jgorset@gmail.com",
   url = "http://github.com/jgorset/django-shortcuts",
