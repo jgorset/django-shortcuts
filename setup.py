11b1b2b96fb63bc07d0ae77cdbdda5fb68fccd1c
Johannes Gorset <jgorset@gmail.com>
2011-12-26 01:10:12 +0100

    Bump version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'django-shortcuts',
-  version = '1.0',
+  version = '1.1',
   description = "You spend way too much time typing 'python manage.py'",
   author = "Johannes Gorset",
   author_email = "jgorset@gmail.com",
