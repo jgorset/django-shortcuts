d6e550f66e5b6936d59055f084629955aae071cb
Johannes Gorset <jgorset@gmail.com>
2012-01-20 23:59:02 +0100

    Fix indents

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 readme = open('README.rst')
 
 setup(
-  name = 'django-shortcuts',
-  version = '1.1',
-  description = "You spend way too much time typing 'python manage.py'",
-  long_description = readme,
-  author = "Johannes Gorset",
-  author_email = "jgorset@gmail.com",
-  url = "http://github.com/jgorset/django-shortcuts",
-  scripts = ['scripts/django']
+    name = 'django-shortcuts',
+    version = '1.1',
+    description = "You spend way too much time typing 'python manage.py'",
+    long_description = readme,
+    author = "Johannes Gorset",
+    author_email = "jgorset@gmail.com",
+    url = "http://github.com/jgorset/django-shortcuts",
+    scripts = ['scripts/django']
 )
